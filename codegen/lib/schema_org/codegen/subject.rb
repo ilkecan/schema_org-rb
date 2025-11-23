25da6d8e4209eea92a913dc6c53c9bc3ff3fb460
ilkecan <ilkecan@protonmail.com>
2025-11-23 16:52:55 +0300

    codegen: Move `Subject::Contract` to own file

--- codegen/lib/schema_org/codegen/subject.rb
+++ codegen/lib/schema_org/codegen/subject.rb
@@ -1,17 +1,8 @@
 require 'dry-initializer'
-require 'dry-validation'
 
 module SchemaOrg
   module Codegen
     module Subject
-      class Contract < Dry::Validation::Contract
-        schema do
-          required(:comment).value(:array, size?: 1)
-          required(:label).value(:array, size?: 1)
-          required(:type).value(:array, min_size?: 1)
-        end
-      end
-
       module_function
 
       def with(prefixes)
---
+++ codegen/lib/schema_org/codegen/subject/contract.rb
@@ -0,0 +1,15 @@
+require 'dry-validation'
+
+module SchemaOrg
+  module Codegen
+    module Subject
+      class Contract < Dry::Validation::Contract
+        schema do
+          required(:comment).value(:array, size?: 1)
+          required(:label).value(:array, size?: 1)
+          required(:type).value(:array, min_size?: 1)
+        end
+      end
+    end
+  end
+end
